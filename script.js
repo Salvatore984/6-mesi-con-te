4c6feada08abf48b316a0e6c1d7fcaa70e4ab58d
Salvatore984 <salvatorecapuano856@gmail.com>
2026-07-12 00:02:25 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function apriLettera(){
+
+    document.querySelector(".busta").style.display="none";
+
+    document.getElementById("messaggio").style.display="block";
+
+    setTimeout(()=>{
+        document.getElementById("lettera").style.background="transparent";
+    },1000);
+
+}

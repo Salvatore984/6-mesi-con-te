de68bbbe6eeff9e0002a15bc6376e9a65c88d4a8
Salvatore984 <salvatorecapuano856@gmail.com>
2026-07-12 00:25:25 +0200

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,61 @@
+// APERTURA LETTERA SEGRETA
+
 function apriLettera(){
 
-    document.querySelector(".busta").style.display="none";
+    document.querySelector(".busta").style.display = "none";
 
-    document.getElementById("messaggio").style.display="block";
+    document.getElementById("messaggio").style.display = "block";
 
-    setTimeout(()=>{
-        document.getElementById("lettera").style.background="transparent";
-    },1000);
+    setTimeout(() => {
+
+        document.getElementById("lettera").style.background = "transparent";
+
+    }, 1000);
 
 }
+
+
+
+
+// CONTATORE TEMPO INSIEME
+
+const dataInizio = new Date("2026-01-12");
+
+
+function aggiornaTempo(){
+
+    const oggi = new Date();
+
+    const differenza = oggi - dataInizio;
+
+
+    const giorni = Math.floor(
+        differenza / (1000 * 60 * 60 * 24)
+    );
+
+
+    const ore = Math.floor(
+        (differenza / (1000 * 60 * 60)) % 24
+    );
+
+
+    const minuti = Math.floor(
+        (differenza / (1000 * 60)) % 60
+    );
+
+
+    const secondi = Math.floor(
+        (differenza / 1000) % 60
+    );
+
+
+    document.getElementById("tempo").innerHTML =
+    `${giorni} giorni ❤️ ${ore} ore ${minuti} minuti ${secondi} secondi`;
+
+}
+
+
+
+setInterval(aggiornaTempo,1000);
+
+aggiornaTempo();
